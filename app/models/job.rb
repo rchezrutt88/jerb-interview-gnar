0d6a627a4a4ab3931e078b6290eb7b774470c60d
RobertRuttenberg <rdruttenberg@gmail.com>
2017-03-10 13:20:42 -0500

    tests pass

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ActiveRecord::Base
+  validates :poster, :location, :status, :category, presence: true
+  validate :status_must_be_new_pending_complete
+
+  def status_must_be_new_pending_complete
+    valid_statuses = ['new', 'pending', 'complete']
+    validates_inclusion_of :status, :in => valid_statuses
+  end
+
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @job = Job.new(poster: 'John Doe', location: 'Swampscott', status: 'new', category: 'pool cleaning')
+  end
+
+  test 'valid job' do
+    assert @job.valid?
+  end
+
+  test 'invalid job without poster' do
+    @job.poster = nil
+    refute @job.valid?, 'saved without poster'
+  end
+
+  test 'invalid job without location' do
+    @job.location = nil
+    refute @job.valid?, 'saved without location'
+  end
+
+  test 'invalid job without status' do
+    @job.status = nil
+    refute @job.valid?, 'saved without status'
+  end
+
+  test 'invalid job with invalid status' do
+    @job.status = 'overdue'
+    refute @job.valid?, 'saved with invalid status'
+  end
+
+  test 'invalid job without category' do
+    @job.category = nil
+    refute @job.valid?, 'saved without category'
+  end
 end
